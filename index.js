d7d7e3b48d98e723f04e85ee75011baffe87edd3
dpunkz09 <dpunkz09@gmail.com>
2026-09-19 00:42:44 +0800

    ...

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ function iframePage(embedUrl) {
     allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
     referrerpolicy="no-referrer"
     scrolling="no"
+    sandbox="allow-scripts allow-same-origin allow-forms allow-popups-to-escape-sandbox allow-presentation"
   ></iframe>
 </body>
 </html>`;
